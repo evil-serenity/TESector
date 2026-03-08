1fa1df111f479c369bbe9251f5a896f7ffe8f79c
JasmineRickards <117599607+JasmineRickards@users.noreply.github.com>
2026-03-08 11:03:51 +0200

    Revert "Added player laws system"

--- Content.Shared/_TE/_NF/Laws/Components/SectorLawsComponent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared._TE.Laws.Systems;
-
-namespace Content.Shared._TE._NF.Laws.Components;
-
-[RegisterComponent]
-public sealed partial class SectorLawsComponent : Component
-{
-    public static List<LawArticle> Articles = new();
-}
